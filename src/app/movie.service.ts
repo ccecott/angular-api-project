d7e80334e51defb367f48d0cc9425cbf7118b944
Katie Gail <katherine.nolde@gmail.com>
2020-05-22 17:25:12 -0400

    mitch help

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -6,21 +6,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MovieService {
   apiKey: string = '120979ac9f0a7e6dbca67ca72dac0636';
-  apiHomeUrl: string =
-    'https://api.themoviedb.org/3/movie/popular';
-  apiTitle: string =
-    'https://api.themoviedb.org/3/search/movie';
-
-  apiDiscover: string =
-    "https://api.themoviedb.org/3/discover/movie"
-  constructor(private http: HttpClient) { }
+  apiHomeUrl: string = 'https://api.themoviedb.org/3/movie/popular';
+  apiTitle: string = 'https://api.themoviedb.org/3/search/movie';
+  apiGenres: string = 'https://api.themoviedb.org/3/genre/movie/list';
+  apiDiscover: string = 'https://api.themoviedb.org/3/discover/movie';
+  constructor(private http: HttpClient) {}
   // popular movies
   getData() {
     return this.http.get(this.apiHomeUrl, {
       params: {
         api_key: this.apiKey,
-        language: "en-US"
-      }
+        language: 'en-US',
+      },
     });
   }
   // keyword
@@ -28,17 +25,24 @@ export class MovieService {
     return this.http.get(this.apiTitle, {
       params: {
         query: myTitle,
-        api_key: this.apiKey
-      }
+        api_key: this.apiKey,
+      },
     });
   }
-  getGenre(genre_ids:any){
-    return this.http.get(this.apiDiscover,{
+  getGenres() {
+    return this.http.get(this.apiGenres, {
       params: {
         api_key: this.apiKey,
-        with_genres: genre_ids
-      }
-    })
-
+      },
+    });
+  }
+  filterMovies(release: any, genre: any) {
+    return this.http.get(this.apiDiscover, {
+      params: {
+        api_key: this.apiKey,
+        primary_release_year: release,
+        with_genres: genre,
+      },
+    });
   }
 }
--- src/app/search-criteria/search-criteria.component.ts
+++ src/app/search-criteria/search-criteria.component.ts
@@ -13,32 +13,62 @@ export class SearchCriteriaComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     private service: MovieService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe((response) => {
-      if (!response.myTitle) {
-        // if not provided a keyword then use my method from my service (getData)
-        this.service.getData().subscribe((response) => {
-          this.data = response;
-        });
-      } else {
-        this.service.getTitle(response.myTitle).subscribe((data) => {
-          this.data = data;
-        });
-      }
-
-    });
-    }
-  titleSearch(form: NgForm) {
-    this.router.navigate(["home"], {
-      queryParams: { myTitle: form.value.title }
-
+    // this.route.queryParams.subscribe((response) => {
+    //   if (!response.myTitle) {
+    //     // if not provided a keyword then use my method from my service (getData)
+    //     this.service.getData().subscribe((response) => {
+    //       this.data = response;
+    //     });
+    //   } else {
+    //     this.service.getTitle(response.myTitle).subscribe((data) => {
+    //       this.data = data;
+    //     });
+    //   }
+    // });
+    // this.route.queryParams.subscribe((response) => {
+    //   if (!response.genre_ids) {
+    //     this.service.getData().subscribe((response) => {
+    //       console.log(response);
+    //       this.data = response;
+    //     });
+    //   } else {
+    //     this.service.getGenre(response.genre_ids).subscribe((data) => {
+    //       this.data = data;
+    //     });
+    //   }
+    // });
+    // this.route.queryParams.subscribe((response) => {
+    //   if (!response.date) {
+    //     this.service.getData().subscribe((response) => {
+    //       console.log(response);
+    //       this.data = response;
+    //     });
+    //   } else {
+    //     this.service.getYear(response.date).subscribe((data) => {
+    //       this.data = data;
+    //     });
+    //   }
+    // });
+    this.service.getGenres().subscribe((response) => {
+      console.log(response);
     });
   }
-  genreDrop(form: NgForm){
-    this.router.navigate(["home"],{
-      queryParams: {genre_ids: form.value.option }
-    })
+  titleSearch(form: NgForm) {
+    this.router.navigate(['home'], {
+      queryParams: { myTitle: form.value.title },
+    });
+  }
+  genreDrop(form: NgForm) {
+    this.router.navigate(['home'], {
+      queryParams: { genre_ids: form.value.genre },
+    });
+  }
+  yearInput(form: NgForm) {
+    this.router.navigate(['home'], {
+      queryParams: { date: form.value.year },
+    });
   }
 }
